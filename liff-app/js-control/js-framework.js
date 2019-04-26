6d0e8ccd33c270741c184d3b65cbd2397189ef63
ito.takahiro <ito.takahiro@linecorp.com>
2019-04-26 19:56:54 +0900

    add js control mode

---
+++ liff-app/js-control/js-framework.js
@@ -0,0 +1,194 @@
+
+class ThingsConn {
+    constructor(device, svUuid, writeUuid, writeIoUuid, readIoUuid) {
+        this.device = device;
+        this.svUuid = svUuid;
+        this.wrUuid = writeUuid;
+        this.wrIoUuid = writeIoUuid;
+        this.rdIoUuid = readIoUuid;
+    }
+
+    async enterBleioMode(){
+        const command = [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+        this.writeCharacteristic(command, 'control');
+    }
+
+    async enterDemoMode(){
+        const command = [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.writeCharacteristic(command, 'control');
+    }
+
+    async writeAdvertUuid(uuid) {
+        const tx_uuid = uuid.replace(/-/g, '');
+        let uuid_byte = [];
+        let hash = 0;
+        for(let i = 0; i < 16; i = i + 1) {
+            uuid_byte[i] = parseInt(tx_uuid.substring(i * 2, i * 2 + 2), 16);
+            hash = hash + uuid_byte[i];
+        }
+        const header = [1, 0, 0, hash];
+        const command = header.concat(uuid_byte);
+        this.writeCharacteristic(command, 'control');
+    }
+
+    async displayWrite(text) {
+        let ch_array = text.split("");
+        for(let i = 0; i < 16; i = i + 1){
+            if(i >= text.length){
+                ch_array[i] = 0;
+            }else{
+                ch_array[i] = (new TextEncoder('ascii')).encode(ch_array[i]);
+            }
+        }
+        const cmd = [1, text.length];
+        const command = cmd.concat(ch_array);
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async displayControl(addr_x, addr_y) {
+        const command = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, addr_x, addr_y];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async displayFontSize(size) {
+        const command = [15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, size];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async displayClear(device) {
+        const command = [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async ledWrite(port, value) {
+        const command = [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async ledWriteByte(value) {
+        const command = [16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, value];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async buzzerControl(value) {
+        const command = [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, value];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async gpioPinMode(port, value) {
+        const command = [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async gpioDigitalWrite(port, value) {
+        const command = [6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async gpioAnalogWrite(port, value) {
+        const command = [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async i2cStartTransmission(address) {
+        const command = [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, address];
+        this.writeCharacteristic(command, 'io');
+    }
+    async i2cWrite(value) {
+        const command = [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, value];
+        this.writeCharacteristic(command, 'io');
+    }
+    async i2cStopTransmission() {
+        const command = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.writeCharacteristic(command, 'io');
+    }
+    async i2cRequestFrom(address, length) {
+        const command = [11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, length, address];
+        this.writeCharacteristic(command, 'io');
+    }
+    async i2cReadRequest(device) {
+        const command = [12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.writeCharacteristic(command, 'io');
+    }
+    async gpioDigitalReadReq(port) {
+        const command = [13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port];
+        this.writeCharacteristic(command, 'io');
+    }
+    async gpioAnalogReadReq(port) {
+        const command = [14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async readReq(cmd) {
+        const command = [32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, cmd];
+        this.writeCharacteristic(command, 'io');
+    }
+
+    async deviceRead() {
+        const readCmdCharacteristic = await this.getCharacteristic(
+            this.device, this.svUuid, this.rdIoUuid);
+
+        const valueBuffer = await this.readCharacteristic(readCmdCharacteristic).catch(e => {
+            onScreenLog('Read Value  : ' + "error");
+            return null;
+        });
+
+        const result = [valueBuffer.getInt16(0, true), valueBuffer.getInt16(2, true)];
+        onScreenLog('Read Value  : ' + result[0] + ", " + result[1]);
+        //return (result[0] * 65536) + result[1];
+        this.readvalue = (result[0] * 65536) + result[1];
+    }
+
+    valueRead(){
+        return this.readvalue;
+    }
+
+    async sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async readCharacteristic(characteristic) {
+        const response = await characteristic.readValue().catch(e => {
+            onScreenLog(`Error reading ${characteristic.uuid}: ${e}`);
+            throw e;
+        });
+        if (response) {
+            onScreenLog(`Read ${characteristic.uuid}: ${buf2hex(response.buffer)}`);
+            const values = new DataView(response.buffer);
+            return values;
+        } else {
+            throw 'Read value is empty?';
+        }
+    }
+
+    async writeCharacteristic(data, mode){
+        let uuid;
+        if(mode == 'control'){
+            uuid = this.wrUuid;
+        }else if(mode == 'io'){
+            uuid = this.wrIoUuid;
+        }else{
+            return;
+        }
+        const characteristic = await this.getCharacteristic(
+              this.device, this.svUuid, uuid);
+        await characteristic.writeValue(new Uint8Array(data)).catch(e => {
+            onScreenLog(`Write value to device ${characteristic.uuid}: ${e}`);
+            throw e;
+        });
+        await this.sleep(10);
+    }
+
+    async getCharacteristic(device, serviceId, characteristicId) {
+        const service = await device.gatt.getPrimaryService(serviceId).catch(e => {
+            flashSDKError(e);
+            throw e;
+        });
+        const characteristic = await service.getCharacteristic(characteristicId).catch(e => {
+            flashSDKError(e);
+            throw e;
+        });
+        onScreenLog(`Got characteristic ${serviceId} ${characteristicId} ${device.id}`);
+        return characteristic;
+    }
+}
---
+++ liff-app/js-control/led-blink/liff-starter.js
@@ -0,0 +1,296 @@
+const USER_SERVICE_UUID = "f2b742dc-35e3-4e55-9def-0ce4a209c552";
+const USER_CHARACTERISTIC_NOTIFY_UUID = "e90b4b4e-f18a-44f0-8691-b041c7fe57f2";
+const USER_CHARACTERISTIC_WRITE_UUID = "4f2596d7-b3d6-4102-85a2-947b80ab4c6f";
+const USER_CHARACTERISTIC_IO_WRITE_UUID = "5136e866-d081-47d3-aabc-a2c9518bacd4";
+const USER_CHARACTERISTIC_IO_READ_UUID = "1737f2f4-c3d3-453b-a1a6-9efe69cc944f";
+
+const deviceUUIDSet = new Set();
+const connectedUUIDSet = new Set();
+const connectingUUIDSet = new Set();
+const notificationUUIDSet = new Set();
+
+let logNumber = 1;
+
+function onScreenLog(text) {
+    const logbox = document.getElementById('logbox');
+    logbox.value += '#' + logNumber + '> ';
+    logbox.value += text;
+    logbox.value += '\n';
+    logbox.scrollTop = logbox.scrollHeight;
+    logNumber++;
+}
+
+window.onload = () => {
+    liff.init(async () => {
+        onScreenLog('LIFF initialized');
+        renderVersionField();
+
+        await liff.initPlugins(['bluetooth']);
+        onScreenLog('BLE plugin initialized');
+
+        checkAvailablityAndDo(() => {
+            onScreenLog('Finding devices...');
+            findDevice();
+        });
+    }, e => {
+        flashSDKError(e);
+        onScreenLog(`ERROR on getAvailability: ${e}`);
+    });
+}
+
+async function checkAvailablityAndDo(callbackIfAvailable) {
+    const isAvailable = await liff.bluetooth.getAvailability().catch(e => {
+        flashSDKError(e);
+        onScreenLog(`ERROR on getAvailability: ${e}`);
+        return false;
+    });
+    // onScreenLog("Check availablity: " + isAvailable);
+
+    if (isAvailable) {
+        document.getElementById('alert-liffble-notavailable').style.display = 'none';
+        callbackIfAvailable();
+    } else {
+        document.getElementById('alert-liffble-notavailable').style.display = 'block';
+        setTimeout(() => checkAvailablityAndDo(callbackIfAvailable), 1000);
+    }
+}
+
+// Find LINE Things device using requestDevice()
+async function findDevice() {
+    const device = await liff.bluetooth.requestDevice().catch(e => {
+        flashSDKError(e);
+        onScreenLog(`ERROR on requestDevice: ${e}`);
+        throw e;
+    });
+    // onScreenLog('detect: ' + device.id);
+
+    try {
+        if (!deviceUUIDSet.has(device.id)) {
+            deviceUUIDSet.add(device.id);
+            addDeviceToList(device);
+        } else {
+            // TODO: Maybe this is unofficial hack > device.rssi
+            document.querySelector(`#${device.id} .rssi`).innerText = device.rssi;
+        }
+
+        checkAvailablityAndDo(() => setTimeout(findDevice, 100));
+    } catch (e) {
+        onScreenLog(`ERROR on findDevice: ${e}\n${e.stack}`);
+    }
+}
+
+// Add device to found device list
+function addDeviceToList(device) {
+    onScreenLog('Device found: ' + device.name);
+
+    const deviceList = document.getElementById('device-list');
+    const deviceItem = document.getElementById('device-list-item').cloneNode(true);
+    deviceItem.setAttribute('id', device.id);
+    deviceItem.querySelector(".device-id").innerText = device.id;
+    deviceItem.querySelector(".device-name").innerText = device.name;
+    deviceItem.querySelector(".rssi").innerText = device.rssi;
+    deviceItem.classList.add("d-flex");
+    deviceItem.addEventListener('click', () => {
+        deviceItem.classList.add("active");
+        try {
+            connectDevice(device);
+        } catch (e) {
+            onScreenLog('Initializing device failed. ' + e);
+        }
+    });
+    deviceList.appendChild(deviceItem);
+}
+
+// Select target device and connect it
+function connectDevice(device) {
+    onScreenLog('Device selected: ' + device.name);
+
+    if (!device) {
+        onScreenLog('No devices found. You must request a device first.');
+    } else if (connectingUUIDSet.has(device.id) || connectedUUIDSet.has(device.id)) {
+        onScreenLog('Already connected to this device.');
+    } else {
+        connectingUUIDSet.add(device.id);
+        initializeCardForDevice(device);
+
+        // Wait until the requestDevice call finishes before setting up the disconnect listner
+        const disconnectCallback = () => {
+            updateConnectionStatus(device, 'disconnected');
+            device.removeEventListener('gattserverdisconnected', disconnectCallback);
+        };
+        device.addEventListener('gattserverdisconnected', disconnectCallback);
+
+        onScreenLog('Connecting ' + device.name);
+        device.gatt.connect().then(() => {
+            updateConnectionStatus(device, 'connected');
+            connectingUUIDSet.delete(device.id);
+
+            const things = new ThingsConn(
+                device,
+                USER_SERVICE_UUID,
+                USER_CHARACTERISTIC_WRITE_UUID,
+                USER_CHARACTERISTIC_IO_WRITE_UUID,
+                USER_CHARACTERISTIC_IO_READ_UUID
+            );
+
+            setup(things);
+            //loop(things);
+        }).catch(e => {
+            flashSDKError(e);
+            onScreenLog(`ERROR on gatt.connect(${device.id}): ${e}`);
+            updateConnectionStatus(device, 'error');
+            connectingUUIDSet.delete(device.id);
+        });
+    }
+}
+
+// Device initialize
+async function setup(things){
+    // Enter to BLE-IO mode
+    await things.enterBleioMode().catch(e => `do not support JS-control mode. please update firmware`);
+    await sleep(1000);
+
+    // Clear display text, ane write new message
+    await things.displayClear();
+    await things.displayControl(0, 0);
+    await things.displayWrite("Hello world");
+
+    // Initial LED
+    await things.ledWrite(2, 1).catch(e => `error: ${e}\n${e.stack}`);
+    await things.ledWrite(3, 1).catch(e => `error: ${e}\n${e.stack}`);
+    await things.ledWrite(4, 1).catch(e => `error: ${e}\n${e.stack}`);
+    await things.ledWrite(5, 1).catch(e => `error: ${e}\n${e.stack}`);
+}
+
+async function loop(things){
+    while(true){
+        // Write LED
+        await things.ledWriteByte(0);
+        await sleep(1000);
+        await things.ledWriteByte(0xff);
+        await sleep(1000);
+    }
+}
+
+// Setup device information card
+function initializeCardForDevice(device) {
+    const template = document.getElementById('device-template').cloneNode(true);
+    const cardId = 'device-' + device.id;
+
+    template.style.display = 'block';
+    template.setAttribute('id', cardId);
+    template.querySelector('.card > .card-header > .device-name').innerText = device.name;
+
+    const thingsUuid = new ThingsConn(
+        device,
+        USER_SERVICE_UUID,
+        USER_CHARACTERISTIC_WRITE_UUID,
+        USER_CHARACTERISTIC_IO_WRITE_UUID,
+        USER_CHARACTERISTIC_IO_READ_UUID
+    );
+
+    template.querySelector('.setuuid').addEventListener('click', () => {
+        thingsUuid.writeAdvertuuid(
+            template.querySelector('.uuid_text').value
+        ).catch(e => onScreenLog(`ERROR on writeAdvertuuid(): ${e}\n${e.stack}`));
+    });
+
+    // Tabs
+    ['write', 'read', 'advert'].map(key => {
+        const tab = template.querySelector(`#nav-${key}-tab`);
+        const nav = template.querySelector(`#nav-${key}`);
+
+        tab.id = `nav-${key}-tab-${device.id}`;
+        nav.id = `nav-${key}-${device.id}`;
+
+        tab.href = '#' + nav.id;
+        tab['aria-controls'] = nav.id;
+        nav['aria-labelledby'] = tab.id;
+    })
+
+    // Remove existing same id card
+    const oldCardElement = getDeviceCard(device);
+    if (oldCardElement && oldCardElement.parentNode) {
+        oldCardElement.parentNode.removeChild(oldCardElement);
+    }
+
+    document.getElementById('device-cards').appendChild(template);
+    onScreenLog('Device card initialized: ' + device.name);
+}
+
+// Update Connection Status
+function updateConnectionStatus(device, status) {
+    if (status == 'connected') {
+        onScreenLog('Connected to ' + device.name);
+        connectedUUIDSet.add(device.id);
+
+        const statusBtn = getDeviceStatusButton(device);
+        statusBtn.setAttribute('class', 'device-status btn btn-outline-primary btn-sm disabled');
+        statusBtn.innerText = "Connected";
+        getDeviceDisconnectButton(device).style.display = 'inline-block';
+        getDeviceCardBody(device).style.display = 'block';
+    } else if (status == 'disconnected') {
+        onScreenLog('Disconnected from ' + device.name);
+        connectedUUIDSet.delete(device.id);
+
+        const statusBtn = getDeviceStatusButton(device);
+        statusBtn.setAttribute('class', 'device-status btn btn-outline-secondary btn-sm disabled');
+        statusBtn.innerText = "Disconnected";
+        getDeviceDisconnectButton(device).style.display = 'none';
+        getDeviceCardBody(device).style.display = 'none';
+        document.getElementById(device.id).classList.remove('active');
+    } else {
+        onScreenLog('Connection Status Unknown ' + status);
+        connectedUUIDSet.delete(device.id);
+
+        const statusBtn = getDeviceStatusButton(device);
+        statusBtn.setAttribute('class', 'device-status btn btn-outline-danger btn-sm disabled');
+        statusBtn.innerText = "Error";
+        getDeviceDisconnectButton(device).style.display = 'none';
+        getDeviceCardBody(device).style.display = 'none';
+        document.getElementById(device.id).classList.remove('active');
+    }
+}
+
+
+
+
+function getDeviceCard(device) {
+    return document.getElementById('device-' + device.id);
+}
+
+function getDeviceCardBody(device) {
+    return getDeviceCard(device).getElementsByClassName('card-body')[0];
+}
+
+function getDeviceStatusButton(device) {
+    return getDeviceCard(device).getElementsByClassName('device-status')[0];
+}
+
+function getDeviceDisconnectButton(device) {
+    return getDeviceCard(device).getElementsByClassName('device-disconnect')[0];
+}
+
+function getDeviceNotificationButton(device) {
+    return getDeviceCard(device).getElementsByClassName('notification-enable')[0];
+}
+
+function renderVersionField() {
+    const element = document.getElementById('sdkversionfield');
+    const versionElement = document.createElement('p')
+        .appendChild(document.createTextNode('SDK Ver: ' + liff._revision));
+    element.appendChild(versionElement);
+}
+
+function flashSDKError(error){
+    window.alert('SDK Error: ' + error.code);
+    window.alert('Message: ' + error.message);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}

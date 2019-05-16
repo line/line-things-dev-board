3a76884b15122ac2b4c762a8e1315b86eb49627b
Hirotaka Kawata <kawata.hirotaka@linecorp.com>
2019-05-16 23:30:18 +0900

    fix js error handling

--- liff-app/js-control/js-framework.js
+++ liff-app/js-control/js-framework.js
@@ -19,72 +19,66 @@ class ThingsIo {
         this.ntfyTempUuid = notifyTempUuid;
     }
 
-    async writeAdvertUuid(uuid){
+    async writeAdvertUuid(uuid) {
         const tx_uuid = uuid.replace(/-/g, '');
         let uuid_byte = [];
         let hash = 0;
-        for(let i = 0; i < 16; i = i + 1) {
+        for (let i = 0; i < 16; i = i + 1) {
             uuid_byte[i] = parseInt(tx_uuid.substring(i * 2, i * 2 + 2), 16);
             hash = hash + uuid_byte[i];
         }
 
         const header = [1, 0, 0, hash];
         const command = header.concat(uuid_byte);
-        await this.writeCharacteristic(command, 'control').catch(e => onScreenLog(`error : writeAdvertUuid()`));
+        await this.writeCharacteristic(command, 'control');
     }
 
-    async tempNotifyEnable(interval, callback){
+    async tempNotifyEnable(interval, callback) {
         const notifyCharacteristic = await this.getCharacteristic(
             this.device, this.svUuid, this.ntfyTempUuid);
 
         notifyCharacteristic.addEventListener('characteristicvaluechanged', callback);
-        await notifyCharacteristic.startNotifications().catch(e => {
-            onScreenLog('notify start error');
-            return null;
-        });
-        onScreenLog('Temperature Notifications STARTED ' + notifyCharacteristic.uuid);
+        await notifyCharacteristic.startNotifications();
+        console.debug('Temperature Notifications STARTED ' + notifyCharacteristic.uuid);
 
         const command = [18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, interval >> 8, interval & 0xff];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : tempNotifyEnable()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
-    async tempNotifyDisable(){
+    async tempNotifyDisable() {
         const command = [18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : tempNotifyDisable()`));
+        await this.writeCharacteristic(command, 'io');
 
         const notifyCharacteristic = await this.getCharacteristic(
             this.device, this.svUuid, this.ntfyTempUuid);
 
         notifyCharacteristic.removeEventListener('characteristicvaluechanged', callback);
-        await notifyCharacteristic.stopNotifications().catch(e => onScreenLog(`error : tempNotifyDisable()`));
-        onScreenLog('Temperature Notifications STOP ' + notifyCharacteristic.uuid);
+        await notifyCharacteristic.stopNotifications();
+        console.debug('Temperature Notifications STOP ' + notifyCharacteristic.uuid);
     }
 
-    async swNotifyEnable(source, mode, interval, callback){
+    async swNotifyEnable(source, mode, interval, callback) {
         const notifyCharacteristic = await this.getCharacteristic(
             this.device, this.svUuid, this.ntfySwUuid);
 
         notifyCharacteristic.addEventListener('characteristicvaluechanged', callback);
-        await notifyCharacteristic.startNotifications().catch(e => {
-            onScreenLog('notify start error');
-            return null;
-        });
-        onScreenLog('SW Notifications STARTED ' + notifyCharacteristic.uuid);
+        await notifyCharacteristic.startNotifications();
+        console.debug('SW Notifications STARTED ' + notifyCharacteristic.uuid);
 
         const command = [17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, source, mode, interval >> 8, interval & 0xff];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error swNotifyEnable()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
-    async swNotifyDisable(){
+    async swNotifyDisable() {
         const command = [17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : swNotifyDisable()`));
+        await this.writeCharacteristic(command, 'io');
 
         const notifyCharacteristic = await this.getCharacteristic(
             this.device, this.svUuid, this.ntfySwUuid);
 
         notifyCharacteristic.removeEventListener('characteristicvaluechanged', callback);
-        await notifyCharacteristic.stopNotifications().catch(e => onScreenLog(`error : swNotifyDisable()`));
-        onScreenLog('SW Notifications STOP ' + notifyCharacteristic.uuid);
+        await notifyCharacteristic.stopNotifications();
+        console.debug('SW Notifications STOP ' + notifyCharacteristic.uuid);
     }
 
     async displayWrite(text) {
@@ -98,129 +92,121 @@ class ThingsIo {
         }
         const cmd = [1, text.length];
         const command = cmd.concat(ch_array);
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : displayWrite()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async displayControl(addr_x, addr_y) {
         const command = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, addr_x, addr_y];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : displayControl()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async displayFontSize(size) {
         const command = [15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, size];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : displayFontSize()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async displayClear() {
         const command = [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : displayClear()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async ledWrite(port, value) {
         const command = [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : ledWrite()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async ledWriteByte(value) {
         const command = [16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : ledWriteByte()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async buzzerControl(value) {
         const command = [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : buzzerControl()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async gpioPinMode(port, value) {
         const command = [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : gpioPinMode()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async gpioDigitalWrite(port, value) {
         const command = [6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : gpioDigitalWrite()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async gpioAnalogWrite(port, value) {
         const command = [7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : gpioAnalogWrite()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async i2cStartTransmission(address) {
         const command = [8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, address];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : i2cStartTransmission()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async i2cWrite(value) {
         const command = [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, value];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : i2cWrite()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async i2cStopTransmission() {
         const command = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : i2cStopTransmission()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async i2cRequestFrom(address, length) {
         const command = [11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, length, address];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : i2cRequestFrom()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async i2cReadRequest(device) {
         const command = [12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : i2cReadRequest()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async gpioDigitalReadReq(port) {
         const command = [13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : gpioDigitalReadReq()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async gpioAnalogReadReq(port) {
         const command = [14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, port];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : gpioAnalogReadReq()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async readReq(cmd) {
         const command = [32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, cmd];
-        await this.writeCharacteristic(command, 'io').catch(e => onScreenLog(`error : readReq()`));
+        await this.writeCharacteristic(command, 'io');
     }
 
     async deviceRead() {
         const readCmdCharacteristic = await this.getCharacteristic(
-            this.device, this.svUuid, this.rdIoUuid).catch(e => onScreenLog(`error : deviceRead(), getCharacteristic`));
+            this.device, this.svUuid, this.rdIoUuid);
 
-        const valueBuffer = await this.readCharacteristic(readCmdCharacteristic).catch(e => {
-            onScreenLog('Read Value  : ' + "error");
-            return null;
-        });
+        const valueBuffer = await this.readCharacteristic(readCmdCharacteristic);
 
         const result = [valueBuffer.getInt16(0, true), valueBuffer.getInt16(2, true)];
-        onScreenLog('Read Value  : ' + result[0] + ", " + result[1]);
+        console.debug('Read Value  : ' + result[0] + ", " + result[1]);
         this.readvalue = (result[0] * 65536) + result[1];
     }
 
-
     async deviceVersionRead() {
         const readCmdCharacteristic = await this.getCharacteristic(
-            this.device, this.svUuid, this.verUuid).catch(e => onScreenLog(`error : deviceVersionRead(), getCharacteristic`));
+            this.device, this.svUuid, this.verUuid);
 
-        const valueBuffer = await this.readCharacteristic(readCmdCharacteristic).catch(e => {
-            onScreenLog('error : deviceVersionRead()');
-            this.version = 1;
-            return null;
-        });
+        const valueBuffer = await this.readCharacteristic(readCmdCharacteristic);
 
         const result = valueBuffer.getInt8(0);
-        onScreenLog('Version : ' + result);
+        console.debug('Version : ' + result);
         this.version = result;
     }
 
-    versionRead(){
+    versionRead() {
         return this.version;
     }
 
-    valueRead(){
+    valueRead() {
         return this.readvalue;
     }
 
@@ -229,12 +215,9 @@ class ThingsIo {
     }
 
     async readCharacteristic(characteristic) {
-        const response = await characteristic.readValue().catch(e => {
-            onScreenLog(`Error reading ${characteristic.uuid}: ${e}`);
-            throw e;
-        });
+        const response = await characteristic.readValue();
         if (response) {
-            onScreenLog(`Read ${characteristic.uuid}: ${buf2hex(response.buffer)}`);
+            console.debug(`Read ${characteristic.uuid}: ${buf2hex(response.buffer)}`);
             const values = new DataView(response.buffer);
             return values;
         } else {
@@ -242,34 +225,24 @@ class ThingsIo {
         }
     }
 
-    async writeCharacteristic(data, mode){
+    async writeCharacteristic(data, mode) {
         let uuid;
-        if(mode == 'control'){
+        if (mode == 'control') {
             uuid = this.wrUuid;
-        }else if(mode == 'io'){
+        } else if (mode == 'io') {
             uuid = this.wrIoUuid;
-        }else{
+        } else {
             return;
         }
-        const characteristic = await this.getCharacteristic(
-              this.device, this.svUuid, uuid);
-        await characteristic.writeValue(new Uint8Array(data)).catch(e => {
-            onScreenLog(`Write value to device ${characteristic.uuid}: ${e}`);
-            throw e;
-        });
+        const characteristic = await this.getCharacteristic(this.device, this.svUuid, uuid);
+        await characteristic.writeValue(new Uint8Array(data));
         await this.sleep(50);
     }
 
     async getCharacteristic(device, serviceId, characteristicId) {
-        const service = await device.gatt.getPrimaryService(serviceId).catch(e => {
-            flashSDKError(e);
-            throw e;
-        });
-        const characteristic = await service.getCharacteristic(characteristicId).catch(e => {
-            flashSDKError(e);
-            throw e;
-        });
-        onScreenLog(`Got characteristic ${serviceId} ${characteristicId} ${device.id}`);
+        const service = await device.gatt.getPrimaryService(serviceId);
+        const characteristic = await service.getCharacteristic(characteristicId);
+        console.debug(`Got characteristic ${serviceId} ${characteristicId} ${device.id}`);
         return characteristic;
     }
 }
